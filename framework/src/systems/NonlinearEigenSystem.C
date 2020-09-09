76a4daae70353f2d6ac5067ee5466f7b9a85d161
Fande Kong <fdkong.jd@gmail.com>
2020-09-09 15:44:23 -0600

    Eigen solver: Remove extra option "-eps_power" from SNES
    since users do not like it

--- framework/src/systems/NonlinearEigenSystem.C
+++ framework/src/systems/NonlinearEigenSystem.C
@@ -443,7 +443,7 @@ NonlinearEigenSystem::getSNES()
   {
     EPS eps = solver->eps();
     SNES snes = nullptr;
-    Moose::SlepcSupport::epsGetSNES(eps, &snes);
+    Moose::SlepcSupport::mooseSlepcEPSGetSNES(eps, &snes);
     return snes;
   }
   else
